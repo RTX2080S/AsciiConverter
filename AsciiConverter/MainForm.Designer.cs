4bb4a4b53b3d6345d5da6996274ca9cc0a639243
Radon222 <nzlili2015@gmail.com>
2015-05-28 01:12:13 +1200

    Upload my project of AsciiConverter

---
+++ AsciiConverter/MainForm.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace AsciiConverter
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.ansBox1 = new System.Windows.Forms.TextBox();
+            this.ansBox2 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.printBox = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.printerEnd = new System.Windows.Forms.NumericUpDown();
+            this.printerStart = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.printerEnd)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.printerStart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(70, 15);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            65535,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(65, 20);
+            this.numericUpDown1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ascii: ";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(141, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "To Char";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Char: ";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(141, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "To Ascii";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(70, 43);
+            this.textBox1.MaxLength = 1;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(65, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // ansBox1
+            // 
+            this.ansBox1.Location = new System.Drawing.Point(222, 14);
+            this.ansBox1.MaxLength = 1;
+            this.ansBox1.Name = "ansBox1";
+            this.ansBox1.ReadOnly = true;
+            this.ansBox1.Size = new System.Drawing.Size(65, 20);
+            this.ansBox1.TabIndex = 9;
+            // 
+            // ansBox2
+            // 
+            this.ansBox2.Location = new System.Drawing.Point(222, 43);
+            this.ansBox2.MaxLength = 1;
+            this.ansBox2.Name = "ansBox2";
+            this.ansBox2.ReadOnly = true;
+            this.ansBox2.Size = new System.Drawing.Size(65, 20);
+            this.ansBox2.TabIndex = 10;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.printBox);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.printerEnd);
+            this.groupBox1.Controls.Add(this.printerStart);
+            this.groupBox1.Location = new System.Drawing.Point(12, 70);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(320, 379);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ascii printer";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(246, 19);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(61, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Stop";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // printBox
+            // 
+            this.printBox.Location = new System.Drawing.Point(6, 74);
+            this.printBox.Multiline = true;
+            this.printBox.Name = "printBox";
+            this.printBox.ReadOnly = true;
+            this.printBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.printBox.Size = new System.Drawing.Size(308, 299);
+            this.printBox.TabIndex = 5;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(179, 19);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(61, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Go";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 50);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "End: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Start:";
+            // 
+            // printerEnd
+            // 
+            this.printerEnd.Location = new System.Drawing.Point(53, 48);
+            this.printerEnd.Maximum = new decimal(new int[] {
+            65535,
+            0,
+            0,
+            0});
+            this.printerEnd.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.printerEnd.Name = "printerEnd";
+            this.printerEnd.Size = new System.Drawing.Size(120, 20);
+            this.printerEnd.TabIndex = 1;
+            this.printerEnd.Value = new decimal(new int[] {
+            127,
+            0,
+            0,
+            0});
+            // 
+            // printerStart
+            // 
+            this.printerStart.Location = new System.Drawing.Point(53, 22);
+            this.printerStart.Maximum = new decimal(new int[] {
+            65535,
+            0,
+            0,
+            0});
+            this.printerStart.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.printerStart.Name = "printerStart";
+            this.printerStart.Size = new System.Drawing.Size(120, 20);
+            this.printerStart.TabIndex = 0;
+            this.printerStart.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(344, 462);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.ansBox2);
+            this.Controls.Add(this.ansBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(360, 500);
+            this.MinimumSize = new System.Drawing.Size(360, 500);
+            this.Name = "mainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ascii Converter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.printerEnd)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.printerStart)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox ansBox1;
+        private System.Windows.Forms.TextBox ansBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown printerEnd;
+        private System.Windows.Forms.NumericUpDown printerStart;
+        private System.Windows.Forms.TextBox printBox;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ AsciiConverter/MainForm.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace AsciiConverter
+{
+    delegate void invokeTextBox(string txt);
+     
+    public partial class mainForm : Form
+    {
+
+        private int counter = 0;
+
+        public void printText(string txt)
+        {
+            if (printBox.InvokeRequired)
+            {
+                invokeTextBox d = new invokeTextBox(printText);
+                this.Invoke(d, txt);
+            }
+            else
+            {
+                printBox.Text += txt;
+                counter += 1;
+                printBox.Select(printBox.Text.Length, 0);
+                printBox.ScrollToCaret();
+            }
+        }
+
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {            
+            char c = (char)numericUpDown1.Value;
+            ansBox1.Text = c.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox1.Text)) return;
+            char c = (char)textBox1.Text[0];
+            ansBox2.Text = ((int)c).ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            printBox.Clear();
+            t = new Thread(fillPrintBox);
+            t.Priority = ThreadPriority.BelowNormal;
+            t.Start();                         
+        }
+
+        public Thread t;
+
+        public void fillPrintBox()
+        {
+            int a = (int)printerStart.Value;
+            int b = (int)printerEnd.Value;
+            if (a <= b)
+            {
+                string cacheStr = "";
+                const int cacheSize = 127;
+                int flushCounter = 0;
+                for (int i = a; i <= b; i++)
+                {
+                    char c = (char)i;
+                    cacheStr += i.ToString() + ": " + c.ToString() + "; ";
+                    flushCounter += 1;
+                    if (flushCounter > cacheSize)
+                    {
+                        printText(cacheStr);
+                        flushCounter = 0;
+                        cacheStr = null;
+                    }
+                }
+                printText(cacheStr);
+            }            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (t != null) t.Abort();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Icon = Properties.Resources.MainIcon;
+        }
+    }
+}
